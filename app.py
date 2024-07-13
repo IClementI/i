ed15ffacf77eef386e12941ea72b0333b5924d14
Clement <clementamar@gmail.com>
2024-07-13 23:09:42 +0200

    l

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, send_file, abort
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/hello/<name>')
-def hello_name(name):
-    return f"Hello, {name}!"
-
-@app.route('/sum/<int:a>/<int:b>')
-def sum_two_numbers(a, b):
-    return f"The sum of {a} and {b} is {a + b}"
+@app.route('/<path:filename>')
+def serve_file(filename):
+    file_path = os.path.join(app.root_path, filename)
+    if os.path.exists(file_path):
+        return send_file(file_path)
+    else:
+        abort(404)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
