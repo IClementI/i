b11c85559cf1703ed1d044748fd75d809abee533
Clement <clementamar@gmail.com>
2024-07-15 18:19:10 +0200

    lol

---
+++ templates/test.py
@@ -0,0 +1,26 @@
+from flask import Flask, send_file, abort
+import os
+
+app = Flask(__name__)
+
+@app.route('/lol/')
+def serve_file(filename):
+    # Print the application's root path
+    print("App root path: " + app.root_path)
+    
+    # Determine and print the requested file path
+    file_path = os.path.join(app.root_path, filename)
+    print("Requested file path: " + file_path)
+    
+    if os.path.exists(file_path):
+        return send_file(file_path)
+    else:
+        abort(404)
+
+@app.route('/')
+def serve_file2():
+	send_file('./index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
